529ace0d7507696317781d9410ce887cda064fc3
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-01-19 11:00:16 -0500

    updates javascript text

---
+++ main.js
@@ -0,0 +1,51 @@
+console.log("Hello")
+
+const firstName ="Gabby"
+
+
+
+const secondName = "Elyse"
+
+console.log(firstName + " " + secondName + " " + "Lowe")
+
+const description = `My name is: ${firstName}
+My middle name: ${secondName}. ${2+2}`
+
+console.log(description)
+
+const age = 9999_99_99
+
+console.log(age)
+
+let isAdmin = true
+//...
+isAdmin = false
+
+const list = [
+    "Gabby"
+]
+
+console.log(list)
+
+const students = [
+    {
+        firstName:"Gabby",
+        lastName:"Elyse",
+        age: 33,
+        isAdmine: true
+    },
+    {
+        firstName:"Lowe",
+        age: 23,
+        isAdmine: true
+    },
+]
+
+console.log (students[1].lastName)
+
+let points = 6
+
+points = points + 5;
+points -= 20.02
+
+console.log(students [1].lastName == "Elyse" || students[1].firstName == "Lowe") 
